bf89fa0e7bcf9ac392e36a4125dea9b07cb67484
Mankevich Nadya <nadya.mankevichhh@mail.ru>
2020-03-24 14:32:10 +0300

    Initial Commit

---
+++ 18dz2_xib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  18dz2_xib
+//
+//  Created by Пользователь on 3/24/20.
+//  Copyright © 2020 Пользователь. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
